a88ad7b2042062554572140c9af90e47037cb866
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2022-12-21 20:01:54 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10;
+            //int y = x++;
+            //int z = ++x;
+            //Console.WriteLine(y);
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a % 5 == 0)
+            //Console.WriteLine("Bolunur");
+
+            //else
+            /*     Console.WriteLine("Bolunumur");  */
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a % 4 == 0 && a % 9 == 0)
+            //    Console.WriteLine("Bolunur");
+            //else
+            //    Console.WriteLine("Bolunmur");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a % 5 == 0)
+            //    Console.WriteLine("Bolunur");
+            //    if (a % 2 == 0)
+            //    Console.WriteLine("Bolunur");
+            //if (a % 3 == 0)
+            //    Console.WriteLine("Bolunur");
+
+            //else
+            //    Console.WriteLine("Bolunmur");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a%7== 0 && a%6== 0 && a%2== 0)
+            //    Console.WriteLine('Bolunur')
+
+            char h = char.Parse(Console.ReadLine());
+            switch (h)
+            {
+                case 'a':
+                case 'b':
+                case 'c':
+                    Console.WriteLine("Saitdir");
+                    break;
+                default:
+                    Console.WriteLine("Sait deyil");
+                    break;
+
+            //        char t = char.Parse(Console.ReadLine());
+            //switch (t)
+            //{ case 't':
+            //    case 'b':
+            //    case 'c':
+            //    case 's':
+            //        Console.WriteLine("Samitdir");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Samit deyil");
+            //        break;
+            }
+
+                        }
+            
+                }
+    }
+}
